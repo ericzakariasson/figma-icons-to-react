a79a02ad6ff6c95b3f3ca73844d8d95a37e07cb5
ericzakariasson <eric.zakariasson@gmail.com>
2020-07-19 20:57:42 +0200

    Update index file

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,117 @@
 import "core-js";
-import { defaultConfig } from "./config";
 import { flatten } from "lodash";
 import chalk from "chalk";
-import fs from "fs";
-import { getConfig } from "./getConfig";
 import { FigmaClient } from "./figma/figma.client";
-import { exportAndGenerateIcon } from "./exportAndGenerateIcon";
-import { IconData, IconExportData } from "./types";
+import {
+  IconData,
+  IconExportData,
+  GeneratorConfig,
+  InvalidGeneratorConfig
+} from "./types";
+import { defaultConfig, validateConfig } from "./config";
+import {
+  generateIcon,
+  generateIndex,
+  generateTypeDeclaration,
+  generateTypes
+} from "./generators";
 
-const main = async () => {
-  const config = getConfig(defaultConfig);
+export const generate = async (
+  initialConfig: GeneratorConfig | InvalidGeneratorConfig = defaultConfig
+) => {
+  const { validationMessages, isValid } = validateConfig(initialConfig);
 
-  if (!fs.existsSync(config.iconsPath)) {
-    throw new Error(
-      `The specified output "${config.iconsPath}" does not exist`
-    );
+  if (!isValid) {
+    validationMessages.forEach(msg => console.log(chalk.red.bold(msg)));
+    process.exit(1);
   }
 
-  const figma = new FigmaClient(config.figmaToken);
+  const config = initialConfig as GeneratorConfig;
 
-  const { nodes } = await figma.getFileNodes(config.fileId, {
-    ids: config.frames
-  });
+  try {
+    const figma = new FigmaClient(config.figma.token);
 
-  const iconData = flatten(
-    Object.values(nodes).map(node => node?.document.children)
-  )
-    .filter(Boolean)
-    .map<IconData | null>(child =>
-      child ? { id: child.id, name: child.name, exportUrl: "" } : null
+    const { nodes } = await figma.getFileNodes(config.figma.fileId, {
+      ids: config.figma.frames
+    });
+
+    const iconData = flatten(
+      Object.values(nodes).map(node => node?.document.children)
     )
-    .filter<IconData>((value): value is IconData => Boolean(value));
+      .filter(Boolean)
+      .map<IconData | null>(child =>
+        child ? { id: child.id, name: child.name, exportUrl: "" } : null
+      )
+      .filter<IconData>((value): value is IconData => Boolean(value));
 
-  const { images } = await figma.getImage(config.fileId, {
-    ids: iconData.map(icon => icon.id),
-    format: "svg"
-  });
+    const { images } = await figma.getImage(config.figma.fileId, {
+      ids: iconData.map(icon => icon.id),
+      format: "svg"
+    });
 
-  const iconExportData = iconData
-    .map<IconExportData>(icon => ({
-      ...icon,
-      url: images[icon.id] ?? ""
-    }))
-    .slice(0, 1);
+    const iconExportData = iconData
+      .map<IconExportData>(icon => ({
+        ...icon,
+        url: images[icon.id] ?? ""
+      }))
+      .slice(0, 1);
 
-  console.log(
-    chalk.green(`Successfully fetched ${iconExportData.length} icons`)
-  );
+    console.log(
+      chalk.green(
+        `Successfully fetched ${iconExportData.length} icon${
+          iconExportData.length === 1 ? "" : "s"
+        } from Figma`
+      )
+    );
 
-  await Promise.all(
-    iconExportData.map(icon => exportAndGenerateIcon(icon, config))
-  );
+    const writtenIcons = await Promise.all(
+      iconExportData.map(icon => generateIcon(icon, config))
+    );
+    console.log(
+      chalk.bold.green(
+        `Successfully generated ${writtenIcons.length} icon${
+          writtenIcons.length === 1 ? "" : "s"
+        }`
+      )
+    );
+
+    const indexFilePath = await generateIndex(writtenIcons, config);
+    console.log(
+      chalk.bold.green(
+        `Successfully generated index file at "${indexFilePath}"`
+      )
+    );
+
+    if (config.typescript) {
+      const typesFilePath = await generateTypes(config);
+      console.log(
+        chalk.bold.green(
+          `Successfully generated types file at "${typesFilePath}"`
+        )
+      );
+    }
+
+    if (config.typeDeclaration) {
+      if (config.typescript && config.typeDeclaration !== "force") {
+        console.log(
+          chalk.yellow(
+            "Do you really want to generate a declaration file (index.d.ts) when using typescript? If yes, set `typeDeclaration` to `force`."
+          )
+        );
+        process.exit(1);
+      }
+      const typeDeclarationFilePath = await generateTypeDeclaration(
+        writtenIcons,
+        config
+      );
+      console.log(
+        chalk.bold.green(
+          `Successfully generated index.d.ts file at "${typeDeclarationFilePath}"`
+        )
+      );
+    }
+  } catch (e) {
+    console.log(chalk.red("An error occured, see details below"));
+    console.error(e);
+  }
 };
-
-main();
